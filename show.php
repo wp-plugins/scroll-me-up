281fca6cd8215186cdc401755f8b7157d5d2ebf8
maluherby <maluherby@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-15 08:15:50 +0000

    Resolve conflict javascript
    git-svn-id: https://plugins.svn.wordpress.org/scroll-me-up/trunk@1160988 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- show.php
+++ show.php
@@ -1,72 +1,33 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-
 <style>
-
 .scroll-up {
-
   width: 50px;
-
   height: 50px;
-
   opacity: 0.7;
-
   position: fixed;
-
   bottom: 50px;
-
   right: 100px;
-
   display: none;
-
   text-indent: -99999px;
-
-  background:url(<?=plugins_url( 'scroll-me-up/scrollup.png', dirname(__FILE__) )?>) no-repeat;
-
-z-index:99999;
-
+  background:url(<?php echo plugins_url( 'scrollup.png', __FILE__ ); ?> ) no-repeat;
+  z-index:99999;
 }
-
 </style>
-
-
-
 <script>
-
   $(document).ready(function () {
-
       $(window).scroll(function () {
-
           if ($(this).scrollTop() > 100) {
-
               $('.scroll-up').fadeIn();
-
           } else {
-
               $('.scroll-up').fadeOut();
-
           }
-
       });
-
       $('.scroll-up').click(function () {
-
           $("html, body").animate({
-
               scrollTop: 0
-
           }, 600);
-
           return false;
-
       });
-
   });
-
 </script>
-
-
-
-
-
 <a href="#" class="scroll-up">scroll</a>
 
